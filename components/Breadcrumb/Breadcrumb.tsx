590448c74f6327c3469df70071a5ad7fd84d57a4
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2022-09-03 16:12:20 +0200

    create Breadcrumb component , add getStatic functon

--- components/Breadcrumb/Breadcrumb.tsx
+++ components/Breadcrumb/Breadcrumb.tsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import Link from 'next/link';
 
-const Breadcrumb = () => {
-  return <div>Breadcrumb</div>;
+type Props = {
+  title: string;
+};
+
+const Breadcrumb = ({ title }: Props) => {
+  return (
+    <div className="bg-zinc-800">
+      <div className="flex items-center max-w-7xl m-auto p-4 text-white text-lg">
+        <Link href="/">
+          <span className="hover: opacity-80 cursor-pointer duration-300">
+            Home
+          </span>
+        </Link>
+        <span className="block px-2">|</span>
+        <span className="font-bold truncate">{title}</span>
+      </div>
+    </div>
+  );
 };
 
 export default Breadcrumb;
--- pages/[id].tsx
+++ pages/[id].tsx
@@ -9,17 +9,24 @@ import Header from '../components/Header/Header';
 import Breadcrumb from '../components/Breadcrumb/Breadcrumb';
 import Grid from '../components/Grid/Grid';
 import Card from '../components/Card/Card';
+import MovieInfo from '../components/MovieInfo/MovieInfo';
 import { basicFetch } from '../api/fetchFunctions';
 import type { GetStaticPaths, GetStaticProps, NextPage } from 'next';
 import type { Movie, Credits, Crew, Cast } from '../api/types';
-import MovieInfo from '../components/MovieInfo/MovieInfo';
 
-const Movie: NextPage = () => {
+type Props = {
+  movie: Movie;
+  directors: Crew[];
+  cast: Cast[];
+};
+
+const Movie: NextPage<Props> = ({ movie, cast, directors }) => {
   return (
     <main>
       <Header />
+      <Breadcrumb title={movie.original_title} />
       <MovieInfo />
-      <Grid>
+      <Grid className="p-4 max-w-7xl m-auto" title="Actors">
         <Card />
       </Grid>
     </main>
@@ -27,3 +34,32 @@ const Movie: NextPage = () => {
 };
 
 export default Movie;
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const id = context.params?.id as string;
+
+  const movieEndpoint: string = movieUrl(id);
+  const creditsEndpoint: string = creditsUrl(id);
+
+  const movie = await basicFetch<Movie>(movieEndpoint);
+  const credits = await basicFetch<Credits>(creditsEndpoint);
+
+  // Get the directors only
+  const directors = credits.crew.filter((member) => member.job === 'Director');
+
+  return {
+    props: {
+      movie,
+      directors,
+      cast: credits.cast,
+    },
+    revalidate: 60 * 60 * 24, // Re-build page every 24 hours
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: 'blocking',
+  };
+};
