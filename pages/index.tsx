831220d5f1b1a057cf2e7393d8166b49fa1eecb3
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2022-09-03 14:01:24 +0200

    create Hero component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next';
+import { IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE } from '../config';
 import Header from '../components/Header/Header';
 import Hero from '../components/Hero/Hero';
 import Grid from '../components/Grid/Grid';
@@ -16,7 +17,19 @@ const Home: NextPage = () => {
   return (
     <main className="relative h-screen overflow-y-scroll">
       <Header setQuery={setQuery} />
-      <Hero />
+      {!query && data && data.pages ? (
+        <Hero
+          imgUrl={
+            data.pages[0].results[0]?.backdrop_path
+              ? IMAGE_BASE_URL +
+                BACKDROP_SIZE +
+                data.pages[0].results[0].backdrop_path
+              : '/no_image.jpg'
+          }
+          title={data.pages[0].results[0].title}
+          text={data.pages[0].results[0].overview}
+        />
+      ) : null}
       <Grid />
       <Card />
       <Spinner />
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        128: '40rem',
+      },
+    },
     fontFamily: {
       raleway: ['Raleway', 'sans-serif'],
     },
